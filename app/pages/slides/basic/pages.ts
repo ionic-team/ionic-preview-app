505b58ee8f1a2cb18f1dbebfc4b4ea33bd82f1d2
Drew Rygh <drew.rygh@gmail.com>
2016-02-07 19:31:36 -0600

    chore(slides): use slides from conf-app
    Related: https://github.com/driftyco/ionic-site/issues/395

--- app/pages/slides/basic/pages.ts
+++ app/pages/slides/basic/pages.ts
@@ -1,4 +1,4 @@
-import {Page} from 'ionic-framework/ionic';
+import {Page, IonicApp, NavController} from 'ionic-framework/ionic';
 import {forwardRef} from 'angular2/core';
 import {AndroidAttribute} from '../../../directives/helpers';
 
@@ -7,7 +7,26 @@ import {AndroidAttribute} from '../../../directives/helpers';
     directives: [forwardRef(() => AndroidAttribute)]
 })
 export class BasicPage {
-  constructor() {
+    constructor(nav: NavController, app: IonicApp) {
+      this.nav = nav;
+      this.app = app;
 
-  }
+      this.slides = [
+        {
+          title: "Welcome to the Docs!",
+          description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+          image: "img/ica-slidebox-img-1.png",
+        },
+        {
+          title: "What is Ionic?",
+          description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+          image: "img/ica-slidebox-img-2.png",
+        },
+        {
+          title: "What is Ionic Platform?",
+          description: "The <b>Ionic Platform</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+          image: "img/ica-slidebox-img-3.png",
+        }
+      ];
+    }
 }

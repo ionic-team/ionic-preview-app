1f67723f7ac40fa2775c1c97802ab59cf3bfeb5a
Brandy Carney <brandyscarney@gmail.com>
2016-02-18 12:08:25 -0500

    demos: update navigation demo to include more languages

--- app/pages/navigation/basic/pages.ts
+++ app/pages/navigation/basic/pages.ts
@@ -8,23 +8,10 @@ import * as helpers from '../../../directives/helpers';
   directives: [forwardRef(() => helpers.AndroidAttribute)]
 })
 class NavigationDetailsPage {
-  constructor(nav: NavController, params: NavParams) {
-    this.nav = nav;
-    this.selection = { title: params.data.name };
-    var navData = {
-        'Angular': "A powerful Javascript framework for building single page apps. Angular is open source, and maintained by Google.",
-        'CSS3': "The latest version of cascading stylesheets - the styling language of the web!",
-        'HTML5': "The latest version of the web's markup language.",
-        'Sass': "Syntactically Awesome Stylesheets - a mature, stable, and powerful professional grade CSS extension."
-    };
-    var navIcons = {
-        'Angular': 'ion-social-angular',
-        'CSS3': 'ion-social-css3',
-        'HTML5': 'ion-social-html5',
-        'Sass': 'ion-social-sass'
-    };
-    this.selection['content'] = navData[this.selection.title];
-    this.selection['icon'] = navIcons[this.selection.title];
+  item;
+
+  constructor(params: NavParams) {
+    this.item = params.data.item;
   }
 }
 
@@ -33,13 +20,69 @@ class NavigationDetailsPage {
     directives: [forwardRef(() => helpers.AndroidAttribute)]
 })
 export class BasicPage {
+  items = [];
 
-  constructor(nav: NavController) {
-      this.nav = nav;
+  constructor(public nav: NavController) {
+    this.items = [
+      {
+        'title': 'Angular',
+        'icon': 'angular',
+        'description': 'A powerful Javascript framework for building single page apps. Angular is open source, and maintained by Google.',
+        'color': '#E63135'
+      },
+      {
+        'title': 'CSS3',
+        'icon': 'css3',
+        'description': 'The latest version of cascading stylesheets - the styling language of the web!',
+        'color': '#0CA9EA'
+      },
+      {
+        'title': 'HTML5',
+        'icon': 'html5',
+        'description': 'The latest version of the web\'s markup language.',
+        'color': '#F46529'
+      },
+      {
+        'title': 'JavaScript',
+        'icon': 'javascript',
+        'description': 'One of the most popular programming languages on the Web!',
+        'color': '#FFD439'
+      },
+      {
+        'title': 'Sass',
+        'icon': 'sass',
+        'description': 'Syntactically Awesome Stylesheets - a mature, stable, and powerful professional grade CSS extension.',
+        'color': '#CE6296'
+      },
+      {
+        'title': 'NodeJS',
+        'icon': 'nodejs',
+        'description': 'An open-source, cross-platform runtime environment for developing server-side Web applications.',
+        'color': '#78BD43'
+      },
+      {
+        'title': 'Python',
+        'icon': 'python',
+        'description': 'A clear and powerful object-oriented programming language!',
+        'color': '#3575AC'
+      },
+      {
+        'title': 'Markdown',
+        'icon': 'markdown',
+        'description': 'A super simple way to add formatting like headers, bold, bulleted lists, and so on to plain text.',
+        'color': '#412159'
+      },
+      {
+        'title': 'Tux',
+        'icon': 'tux',
+        'description': 'The official mascot of the Linux kernel!',
+        'color': '#000'
+      },
+    ]
   }
 
   openNavDetailsPage(item) {
-    this.nav.push(NavigationDetailsPage, { name: item });
+    this.nav.push(NavigationDetailsPage, { item: item });
   }
 
 }

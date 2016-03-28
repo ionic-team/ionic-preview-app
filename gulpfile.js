fcd9579ec9cf69a92d474a3617a9d5d427043ebe
perry <perry@ionic.io>
2016-03-28 14:58:56 -0500

    ionic-site testing gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var del = require('del');
+var exec = require('child_process').exec;
+var config = require('./scripts/config.json');
 
 /**
  * Ionic Gulp tasks, for more information on each see
@@ -21,4 +23,12 @@ gulp.task('html', copyHTML);
 gulp.task('fonts', copyFonts);
 gulp.task('clean', function(done) {
   del('www/build', done);
+});
+
+/* FOR IONIC SITE DEBUGGING ONLY */
+gulp.task('site-watch', ['site-copy'], function() {
+  gulp.watch('app/**/*', ['site-copy']);
+});
+gulp.task('site-copy', ['build'], function() {
+  exec('cp -R www ' + config.demoDest);
 });

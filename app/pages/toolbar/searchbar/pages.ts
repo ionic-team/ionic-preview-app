1cddded5a48a51cb6d4bdc33164c2fb027bc242d
Ionitron <hi@ionicframework.com>
2016-04-01 09:59:02 -0500

    add toolbar searchbar example

--- app/pages/toolbar/searchbar/pages.ts
+++ app/pages/toolbar/searchbar/pages.ts
@@ -4,4 +4,42 @@ import {Page, Platform} from 'ionic-angular';
 @Page({
     templateUrl: './build/pages/toolbar/searchbar/template.html'
 })
-export class SearchbarPage { }
+export class SearchbarPage {
+  constructor() {
+    this.searchQuery = '';
+    this.initializeItems();
+  }
+
+  initializeItems() {
+    this.items = [
+      'Angular 1.x',
+      'Angular 2',
+      'ReactJS',
+      'EmberJS',
+      'Meteor',
+      'Typescript',
+      'Dart',
+      'CoffeeScript'
+    ];
+  }
+
+  getItems(searchbar) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set q to the value of the searchbar
+    var q = searchbar.value;
+
+    // if the value is an empty string don't filter the items
+    if (q.trim() == '') {
+      return;
+    }
+
+    this.items = this.items.filter((v) => {
+      if (v.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+        return true;
+      }
+      return false;
+    })
+  }
+}

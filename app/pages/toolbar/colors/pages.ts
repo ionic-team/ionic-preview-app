f4fa2c9a5071835ad64ca466e029c57632c7d8ba
Brandy Carney <brandyscarney@gmail.com>
2016-04-26 18:47:11 -0400

    fix(toolbar): add padding and update status bar background color
    fixes the show/hideWhen
    
    references driftyco/ionic-site#580

--- app/pages/toolbar/colors/pages.ts
+++ app/pages/toolbar/colors/pages.ts
@@ -2,35 +2,40 @@ import {Page, NavController} from 'ionic-angular';
 
 @Page({
   template: `
-      <ion-navbar *navbar dark>
-      <ion-title>Dark</ion-title>
-      </ion-navbar>
-      <ion-content>
-      </ion-content>
+    <ion-navbar *navbar light>
+      <ion-title>Toolbar: Light</ion-title>
+    </ion-navbar>
+    <ion-content padding>
+      <button block (click)="goBack()">Go Back to Beginning</button>
+    </ion-content>
   `
 })
 export class Page4 {
-  constructor(
-    public nav: NavController
-  ) { }
+  constructor(public nav: NavController) {
+
+  }
+
+  goBack() {
+    this.nav.setRoot(ColorsPage);
+  }
 }
 
 
 @Page({
   template: `
-      <ion-navbar *navbar danger>
-      <ion-title>Danger</ion-title>
-      </ion-navbar>
+    <ion-navbar *navbar danger>
+      <ion-title>Toolbar: Danger</ion-title>
+    </ion-navbar>
 
-      <ion-content>
-      <button block (click)="pushPage()">Go to new page</button>
-      </ion-content>
+    <ion-content padding>
+      <button block (click)="pushPage()">Next Page</button>
+    </ion-content>
   `
 })
 export class Page3 {
-  constructor(
-    public nav: NavController
-  ) { }
+  constructor(public nav: NavController) {
+
+  }
 
   pushPage() {
     this.nav.push(Page4);
@@ -40,19 +45,19 @@ export class Page3 {
 
 @Page({
   template: `
-      <ion-navbar *navbar secondary>
-      <ion-title>Secondary</ion-title>
-      </ion-navbar>
+    <ion-navbar *navbar secondary>
+      <ion-title>Toolbar: Secondary</ion-title>
+    </ion-navbar>
 
-      <ion-content>
-      <button block (click)="pushPage()">Go to new page</button>
-      </ion-content>
+    <ion-content padding>
+      <button block (click)="pushPage()">Next Page</button>
+    </ion-content>
   `
 })
 export class Page2 {
-  constructor(
-    public nav: NavController
-  ) { }
+  constructor(public nav: NavController) {
+
+  }
 
   pushPage() {
     this.nav.push(Page3);
@@ -63,19 +68,19 @@ export class Page2 {
 
 @Page({
   template: `
-      <ion-navbar *navbar primary>
-      <ion-title>Primary</ion-title>
-      </ion-navbar>
+    <ion-navbar *navbar primary>
+      <ion-title>Toolbar: Primary</ion-title>
+    </ion-navbar>
 
-      <ion-content>
-      <button block (click)="pushPage()">Go to new page</button>
-      </ion-content>
+    <ion-content padding>
+      <button block (click)="pushPage()">Next Page</button>
+    </ion-content>
   `
 })
 export class ColorsPage {
-  constructor(
-    public nav: NavController
-  ) { }
+  constructor(public nav: NavController) {
+
+  }
 
   pushPage() {
     this.nav.push(Page2);

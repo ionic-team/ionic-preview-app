ce7b1d7d852624abd2b4e86b979a5dc5c5a76988
Brandy Carney <brandyscarney@gmail.com>
2016-11-28 13:28:18 -0500

    refactor(lists): update list template to use buttons and clicks

--- src/pages/lists/basic/pages.ts
+++ src/pages/lists/basic/pages.ts
@@ -2,6 +2,30 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    templateUrl: 'template.html'
+  templateUrl: 'template.html'
 })
-export class BasicPage { }
+export class BasicPage {
+  items = [
+    'Pokémon Yellow',
+    'Super Metroid',
+    'Mega Man X',
+    'The Legend of Zelda',
+    'Pac-Man',
+    'Super Mario World',
+    'Street Fighter II',
+    'Half Life',
+    'Final Fantasy VII',
+    'Star Fox',
+    'Tetris',
+    'Donkey Kong III',
+    'GoldenEye 007',
+    'Doom',
+    'Fallout',
+    'GTA',
+    'Halo'
+  ];
+
+  itemSelected(item: string) {
+    console.log("Selected Item", item);
+  }
+}
--- src/pages/lists/no-lines/pages.ts
+++ src/pages/lists/no-lines/pages.ts
@@ -2,6 +2,30 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    templateUrl: 'template.html'
+  templateUrl: 'template.html'
 })
-export class NoLinesPage { }
+export class NoLinesPage {
+  items = [
+    'Pokémon Yellow',
+    'Super Metroid',
+    'Mega Man X',
+    'The Legend of Zelda',
+    'Pac-Man',
+    'Super Mario World',
+    'Street Fighter II',
+    'Half Life',
+    'Final Fantasy VII',
+    'Star Fox',
+    'Tetris',
+    'Donkey Kong III',
+    'GoldenEye 007',
+    'Doom',
+    'Fallout',
+    'GTA',
+    'Halo'
+  ];
+
+  itemSelected(item: string) {
+    console.log("Selected Item", item);
+  }
+}

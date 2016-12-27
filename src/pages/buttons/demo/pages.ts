8a7ed23f273ec9b16464a8da81a10efc8ba30a80
Alex Muramoto <amuramoto@apache.org>
2016-12-27 11:39:13 -0800

    refactor(button): rename template file and add header button

--- src/pages/buttons/demo/pages.ts
+++ src/pages/buttons/demo/pages.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 
 @Component({
-  templateUrl: 'basic.html'
+  templateUrl: 'demo.html'
 })
-export class BasicPage { }
+export class ButtonsDemoPage { }
